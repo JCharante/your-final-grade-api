3885dc57eb8d0a018d8acf4017ed89d8e39579fc
jcharante <git@jcharante.com>
2019-10-01 17:20:43 -0400

    add authcode

--- index.js
+++ index.js
@@ -22,13 +22,59 @@ async function handleRequest(req, res, spare) {
     
     let requestType = ""
     requestType = body.requestType || requestType;
+    let userDoc;
+    
+    // enforce a session key for non-login, non-signup, or non-ping requests
+    if (requestType !== "ping" || requestType !== "login" || requestType !== "signup") {
+        const sessionKey = body.sessionKey;
+        if (!sessionKey) {
+            res.status(403).send('You must include a session key in your request');
+            return;
+        }
+        // there is a session key
+        // get user for session key
+        try {
+            userDoc = await mongoHandler.getUserFromSessionKey(sessionKey);
+        } catch (err) {
+            res.status(400).send(err.toString())
+        }
+    }
     
     try {
-        let userId;
-        let locationId;
+        const username = body.username;
+        const password = body.password;
         switch (requestType) {
             case 'ping':
                 res.status(200).send('pong');
+                break;
+            case 'getUserDetails':
+                res.status(200).send(JSON.stringify({
+                    username: userDoc.username,
+                    displayName: userDoc.diplayName
+                }))
+                break;
+            case 'signup':
+                const displayName = body.displayName;
+                try {
+                    userDoc = await mongoHandler.createUser(username, password, displayName);
+                } catch (error) {
+                    res.status(400).send(error.toString());
+                    return;
+                }
+                res.status(200).send(JSON.stringify({
+                    sessionKey: await mongoHandler.createSession(userDoc.username)
+                }))
+                break;
+            case 'login':
+                try {
+                    userDoc = await mongoHandler.verifyPassword(username, password);
+                } catch (error) {
+                    res.status(403).send(error.toString());
+                    return;
+                }
+                res.status(200).send(JSON.stringify({
+                    sessionKey: await mongoHandler.createSession(userDoc.username)
+                }));
                 break;
             default:
                 res.status(400).send(`Unsupported requestType "${requestType}"`);
--- mongo.js
+++ mongo.js
@@ -1,2 +1,73 @@
-module.exports = {}
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const atlasURL = process.env.ATLAS_URL;
 
+module.exports = {
+    getUserFromSessionKey: async function(sessionKey) {
+        const client = await new MongoClient(atlasURL, { useNewUrlParser: true });
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db('your-final-grade');
+        const sessionsCollection = db.collection('sessions');
+        
+        const sessionDocument = await sessionsCollection.findOne({ _id: ObjectId(sessionKey) });
+        if (!sessionDocument) {
+            throw new Error('Invalid Session Key');
+        }
+        
+        const usersCollection = db.collection('users');
+        const userDocument = await usersCollection.findOne({ _id: sessionDocument.user });
+        if (!userDocument) {
+            throw new Error('User Does Not Exist');
+        }
+        await client.close();
+        return userDocument;
+    },
+    createUser: async function(username, password, displayName) {
+        const client = await new MongoClient(atlasURL, { useNewUrlParser: true });
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db('your-final-grade');
+        const usersCollection = db.collection('users');
+        // verify username is unique
+        let doc = await usersCollection.findOne({ username });
+        if (doc) {
+            throw new Error('Username taken');
+        }
+        await usersCollection.insertOne({
+            username,
+            password,
+            displayName
+        })
+        const userDocument = await usersCollection.findOne({ username, password, displayName });
+        await client.close();
+        return userDocument;
+    },
+    createSession: async function(userId) {
+        const client = await new MongoClient(atlasURL, { useNewUrlParser: true });
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db('your-final-grade');
+        const sessionsCollection = db.collection('sessions');
+        const ret = await sessionsCollection.insertOne({ user: userId })
+        await client.close();
+        return { sessionKey: ret.insertedId.toString(), userId: userId.toString() };
+    },
+    verifyPassword: async function(username, password) {
+        const client = await new MongoClient(atlasURL, {useNewUrlParser: true});
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db('your-final-grade');
+        const usersCollection = db.collection('users');
+        const userDocument = await usersCollection.findOne({username});
+        await client.close();
+        if (!userDocument) {
+            throw new Error('Invalid Credentials');
+        }
+        if (userDocument.password === password) {
+            return userDocument;
+        } else {
+            throw new Error('Invalid Credentials');
+        }
+    },
+}
